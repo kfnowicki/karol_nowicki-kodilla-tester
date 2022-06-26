29026c2cd5647ea2ae8261bd964699cdaf8abe14
Karol Nowicki <kfnowicki@gmail.com>
2022-06-26 13:03:01 +0200

    MainExtended added

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Karol");
+        System.out.println(34);
+        System.out.println('N');
+    }
 }
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main (String[] args) {
+       String myText = "Hej, tu Karol";
+       int moduleNumber = 0;
+       double piRounded = 3.14;
+       char firstLetterName = 'K';
+       boolean amIDoingItRight = true;
+       String exerciseText = "Hello MainExtended";
+       System.out.println(myText);
+       System.out.println(moduleNumber);
+       System.out.println(piRounded);
+       System.out.println(firstLetterName);
+       System.out.println(amIDoingItRight);
+       System.out.println(exerciseText);
+    }
+}
