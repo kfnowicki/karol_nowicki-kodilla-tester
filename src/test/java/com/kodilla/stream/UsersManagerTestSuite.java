d679029527bb35750c8b041b5df553e3537a5b90
Karol Nowicki <kfnowicki@gmail.com>
2022-08-30 00:35:55 +0200

    Optional

--- src/test/java/com/kodilla/stream/UsersManagerTestSuite.java
+++ src/test/java/com/kodilla/stream/UsersManagerTestSuite.java
@@ -3,16 +3,17 @@ package com.kodilla.stream;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UsersManagerTest {
+class UsersManagerTestSuite {
 
     @Test
     void testFilterChemistGroupUsernames() {
         List<String> result = UsersManager.filterChemistGroupUsernames();
-        List expected = new ArrayList<>();
+        List<String> expected = new ArrayList<>();
         expected.add("Walter White");
         expected.add("Gale Boetticher");
         assertEquals(result,expected);
@@ -32,5 +33,18 @@ class UsersManagerTest {
 
         assert (expected);
         assertEquals(result.size(),counter);
+    }
+
+    @Test
+    void testFindMostActiveUsers(){
+        List <String> importedList = UsersManager.findMostActiveUsers();
+        List <Integer> extractedList = new ArrayList<>();
+        for (String activeUser : importedList)
+            extractedList.add(Integer.parseInt(activeUser.replaceAll("[^0-9]", "")));
+        List<Integer> sortedList = new ArrayList<Integer>(extractedList);
+        Collections.sort(sortedList);
+        Collections.reverse(sortedList);
+
+        assertEquals(extractedList,sortedList);
     }
 }
